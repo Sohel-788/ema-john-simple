5ea3132634b3307659dfdb13973a9f28e2b35b2c
Sohel-788 <mahmud3724@gmail.com>
2020-08-27 08:52:31 -0500

    Ema-john-simple

--- src/App.js
+++ src/App.js
@@ -6,21 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello React! I will capture you as soon as possible</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// copy paste the below code in product.js file and npm start 
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
